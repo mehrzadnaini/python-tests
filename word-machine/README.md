dfc782881983acc46ea70ffcdf79b28702b70963
Mehrzad Naini <Mehrzad@qadmins-MacBook-Pro.local>
2022-09-28 10:07:40 +0200

    add word machine basic structure

---
+++ word-machine/README.md
@@ -0,0 +1,1 @@
+A word machine is a system that performs a sequence of simple operations on a stack of integers. Initially the stack is empty. The sequence of operations is given as a string. Operations are separated by single spaces. The following operations may be specified:
---
+++ word-machine/word-machine.py
@@ -0,0 +1,69 @@
+class WordMachine:
+    def __init__(self):
+        self.result = []
+        self.error = ''
+
+    def opperation_error(self):
+        self.error = 'Operations need at least two inputs'
+
+    def push(self,input):
+        self.result.append(input)
+    
+    def pop(self):
+        del self.result[-1]
+    
+    def duplicate(self):
+        if not len(self.result) > 0:
+            self.error = 'List is empty / impossible opperation'
+            return 
+        self.result.append(self.result[-1])
+
+    def sub(self):
+        if not len(self.result)> 1:
+            self.opperation_error()
+            return
+        r = self.result[-2] - self.result[-1]
+        result = self.result[:-2]
+        result.append(r)
+        self.result = result
+        
+    def add(self):
+        if not len(self.result)> 1:
+            self.opperation_error()
+            return
+        r = self.result[-2] + self.result[-1]
+        result = self.result[:-2]
+        result.append(r)
+        self.result = result
+    
+    def reset(self):
+        self.result = []
+        self.error = ''
+
+    def interpreter(self,op):
+        if op == "+":
+            self.add()
+            return
+        if op == "-":
+            self.sub()
+            return
+        if op.isnumeric():
+            self.push(int(op))
+            return
+        if op == "POP":
+            self.pop()
+        if op == "DUP":
+            self.duplicate()
+
+
+    def get_result(self,input):
+        self.reset()
+        input_list = input.split(" ")
+        for op in input_list:
+            self.interpreter(op)
+        return self.result
+
+
+word_machine = WordMachine()
+print(word_machine.get_result("4 5 6 - 7 +"))
+print(word_machine.get_result("4 5 6 POP +"))
